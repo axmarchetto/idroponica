19b65e9d24625bb4989b7c1034a1fd9ef7183a34
andrea <a_marchetto@libero.it>
2020-12-14 22:53:55 +0100

    inital commit

--- multithread.py
+++ multithread.py
@@ -19,10 +19,3 @@ def stampa_uscita_funz(s):
 def sottoprog_completato():
     print("THREAD COMPLETE!")
 
-
-def gest_multithread(self):
-    worker = Worker(self.execute_this_fn)
-    worker.signals.result.connect(self.print_output)
-    worker.signals.finished.connect(self.thread_complete)
-    worker.signals.progress.connect(self.progress_fn)
-    self.threadpool.start(worker)
